043d3f2ac138650f6c629d50cf2ab232124f83ae
Jean Martins <jeanmrtns4@gmail.com>
2021-04-14 21:43:37 -0300

    Profile

--- src/routes.js
+++ src/routes.js
@@ -3,10 +3,19 @@ const routes = express.Router()
 
 const views = __dirname + '/views/'
 
+const profile = {
+  name: 'Jean',
+  avatar: 'https://avatars.githubusercontent.com/u/55639735?v=4',
+  "monthly-budget": 3000,
+  "hours-per-day": 5,
+  "days-per-week": 5,
+  "vacation-per-year": 4,
+}
+
 // Request, Response => Cria as rotas da aplicação
 routes.get('/', (req, res) => res.render(views + 'index'))
 routes.get('/job', (req, res) => res.render(views + 'job'))
 routes.get('/job/edit', (req, res) => res.render(views + 'job-edit'))
-routes.get('/profile', (req, res) => res.render(views + 'profile'))
+routes.get('/profile', (req, res) => res.render(views + 'profile', { profile }))
 
 module.exports = routes
